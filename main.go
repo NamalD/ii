63b702c939b07488f2b4efda08483c97a62b2e5b
Namal Dayarathna <dayarathnand@gmail.com>
2021-02-03 20:00:25 +0000

    Load image from args

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-    "fyne.io/fyne/v2"
-    "fyne.io/fyne/v2/app"
-    "fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"os"
 )
 
 func main() {
-    // TODO: Load image from arg
-    // TODO: Keybindings
-    // TODO: Next/previous image in folder
-    // TODO: Hide window borders
+	// TODO: Keybindings
+	// TODO: Next/previous image in folder
+	// TODO: Hide window borders
+	if len(os.Args) < 2 {
+		return
+	}
 
-    app := app.New()
-    window := app.NewWindow("Hello")
+	iiApp := app.New()
+	window := iiApp.NewWindow("ii")
 
-    image := canvas.NewImageFromFile("P1200182.png")
-    image.FillMode = canvas.ImageFillOriginal
-    window.SetContent(image)
+	filename := os.Args[1]
+	window.SetTitle("ii: " + filename)
+	image := canvas.NewImageFromFile(filename)
+	image.FillMode = canvas.ImageFillOriginal
+	window.SetContent(image)
 
-    window.Resize(fyne.NewSize(800, 600))
-    window.ShowAndRun()
+	window.ShowAndRun()
 }
